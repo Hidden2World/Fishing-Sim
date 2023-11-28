0f367c7def6cd39d5334a14949ff2dddcdd42ae1
Dinky7897 <bothma2006@gmail.com>
2023-11-28 21:02:18 +1100

    Spring Joint

---
+++ Fishing/Assets/HookMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookMovement : MonoBehaviour
+{
+    public GameObject watercollider;
+    public Rigidbody rb;
+    public float waterGravity;
+    public float aboveGravity;
+    bool inWater;
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waterGravity = waterGravity * 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        if (inWater)
+        {
+            rb.AddForce(0, -waterGravity * Time.deltaTime, 0);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == watercollider)
+        {
+            Debug.Log("in water");
+            inWater = true;
+            
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        inWater = false;
+    }
+
+
+}
