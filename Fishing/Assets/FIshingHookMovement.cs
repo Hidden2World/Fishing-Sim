49bd4fa135b21044109096ff29c1c4dede7a694b
Dinky7897 <bothma2006@gmail.com>
2023-11-30 19:01:17 +1100

    Bad movement prototype though movement none the less

--- Fishing/Assets/FIshingHookMovement.cs
+++ Fishing/Assets/FIshingHookMovement.cs
@@ -11,8 +11,15 @@ public class FIshingHookMovement : MonoBehaviour
     public float jointDecrease;
     public SpringJoint joint;
     bool down;
-    bool inWater; 
-
+    bool up;
+    public bool inWater;
+    public GameObject waterCollider;
+    public Rigidbody rb;
+    public float leftMove;
+    public float rightMove;
+    bool left;
+    bool right;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -24,47 +31,101 @@ public class FIshingHookMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (inWater)
-            while (Input.GetMouseButtonDown(0))
-            {
-                Debug.Log("mousebutton");
-                down = false;
-            }
-            else
-                down = true;
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("a");
+            left = true;
+        }
+        else
+        {
+            left = false;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            right = true;
+            Debug.Log("d");
+
+        }
+        else
+        {
+            right = false;
+        }
+        /*if (Input.GetKeyDown("right"))
+        {
+            rb.AddForce(rightMove, 0, 0);
+            Debug.Log("right");
+        }
+        */
+        // Debug.Log("inwater");
+        if (Input.GetMouseButton(0) && inWater)
+        {
+            Debug.Log("mouse button going up");
+            down = false;
+            up = true;
+        }
+        else
+        {
+            down = true;
+            up = false;
+        }
+        // if (!down && Input.GetMouseButtonUp(0))
+        //{
+
+        //down = true;
+        // }
+
+
         if (down)
         {
-            Debug.Log("down is ture");
+            //Debug.Log("down is ture");
+        }
+    }
+    
+
+        void FixedUpdate()
+        {
+        if (left)
+        {
+            // rb.AddForce(-leftMove, 0, 0);
+            //Vector3 transform = new Vector3(0, 0, 0,);
+            transform.position += new Vector3(-leftMove, 0, 0);
+            Debug.Log("lefdt");
+        }
+        if (right)
+        {
+            //rb.AddForce(rightMove, 0, 0);
+            transform.position += new Vector3(rightMove, 0, 0);
+            Debug.Log("right");
+            
         }
 
-    }
 
-    private void FixedUpdate()
-    {
         if (down)
         {
             joint.maxDistance = joint.maxDistance + jointIncrease * Time.deltaTime;
+            //rb.AddForce(0, -1, 0);
             Debug.Log("goingdown");
+            
         }
-        if (!down)
+        if (up)
         {
-            joint.maxDistance = -jointDecrease * Time.deltaTime;
+            joint.maxDistance = joint.maxDistance - jointDecrease * Time.deltaTime;
             Debug.Log("going up");
         }
     }
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.name == "water")
+        if (collision.gameObject == waterCollider)
         {
             inWater = true; 
         }
     }
-    private void OnCollisionExit(Collision collision) 
+    private void OnTriggerExit(Collider collision)
     {
-        if (inWater)
-        {
-            inWater = false; 
-        }
+        //if (inWater)
+       // {
+        //    inWater = false; 
+        //
     }
 }
 
