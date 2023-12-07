9e66df113f6333d5e92c225bd86c0ea1856b612e
Assammilk <120110580+Assammilky@users.noreply.github.com>
2023-12-07 23:23:56 +1100

    PAUSE MENU
    Added:
    - pause menu script
    - pause menu ui
    - pause menu prefabs to add into samplescene

---
+++ Fishing/Assets/Scripts/UI Scripts/PauseMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class PauseMenu : MonoBehaviour
+//Naomi
+{
+    [SerializeField] GameObject pauseMenu = null;
+
+    bool isPaused;
+
+    public bool IsPaused { get { return isPaused; } }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+            Time.timeScale = isPaused ? 0 : 1;
+            pauseMenu.SetActive(isPaused);
+        }
+    }
+}
