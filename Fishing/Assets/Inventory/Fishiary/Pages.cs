c3e565bcf247f43540d32f7275492f6efe752fed
Hidden2World <camdenbeyer@gmail.com>
2023-12-19 18:12:26 +1100

    Updated Fishiary
    This is adding what it will look like. The icons and stuff will obviously need upgrading like polishing and fixing but these are in place for what it should look like. You can't go passed the set amount of pages now.

--- Fishing/Assets/Inventory/Fishiary/Pages.cs
+++ Fishing/Assets/Inventory/Fishiary/Pages.cs
@@ -1,31 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Pages : MonoBehaviour
 {
 
     public Transform[] page;
-    private int pageNumber ; //has to be an int!
+    public TMP_Text PageNumber;
+    private int pageNumberInt ; //has to be an int!
 
 
     private void Start()
     {
-        pageNumber = 0;
+        pageNumberInt = 0;
+       
+    }
+
+    private void Update()
+    {
+        
     }
 
 
     public void Next()
     {
-        
-        page[pageNumber].gameObject.SetActive(false);
-        pageNumber++;
-        page[pageNumber].gameObject.SetActive(true);
+        if (pageNumberInt < 19)
+        {
+            page[pageNumberInt].gameObject.SetActive(false);
+            pageNumberInt++;
+            PageNumber.text = $"Page: {pageNumberInt + 1}";
+            page[pageNumberInt].gameObject.SetActive(true);
+        }
     }
     public void Back()
     {
-        page[pageNumber].gameObject.SetActive(false);
-        pageNumber--;
-        page[pageNumber].gameObject.SetActive(true);
+        if (pageNumberInt >= 1)
+        {
+            page[pageNumberInt].gameObject.SetActive(false);
+            pageNumberInt--;
+            PageNumber.text = $"Page: {pageNumberInt + 1}";
+            page[pageNumberInt].gameObject.SetActive(true);
+        }
+   
     }
 }
