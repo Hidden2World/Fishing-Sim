db026dc0f50f60bd1c3fae3f1ce6b32bcbbe9108
Susan Achiech <127722179+SusanAchiech@users.noreply.github.com>
2024-01-09 09:12:13 +0300

    Ocean map

---
+++ Fishing/Assets/WaveStrength.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveStrength : MonoBehaviour
+{
+    public float WaterNoiseScale;
+    public float WaterHeight;
+    public float WaterSpeed;
+    public float FoamDepth;
+    public float FoamRippleSpeed;
+    public float FoamNoiseScale;
+    public float DepthFade;
+    //public gameObject collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
