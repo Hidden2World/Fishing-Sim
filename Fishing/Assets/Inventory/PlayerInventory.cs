e91f794b1cdfd1a203627a276bca687d7e06aaf9
300mice <ben@sacada.net>
2024-01-18 14:23:04 +1100

    REMOVE IF CONFLICTING - fixed inventory persistence bug.
    It was a 50/50 whether the fish would stay in the inventory when transitioning between scenes. It should now be permanent!
    - remove if you have conflicts!

--- Fishing/Assets/Inventory/PlayerInventory.cs
+++ Fishing/Assets/Inventory/PlayerInventory.cs
@@ -20,7 +20,7 @@ public class PlayerInventory : MonoBehaviour
 
     }
 
-    static private PlayerInventory playerinventory = null;
+    private static PlayerInventory playerinventory;
 
     private void Awake() // called when the object is created (before start)
     {
